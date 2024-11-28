a1c3d1f99ee3ff7864bb11d602f60437373997cd
Thiago <thiagosilva1@unipam.edu.br>
2024-11-28 14:15:41 -0300

    feat(deploy): ajustando arquivos para o deploy

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,6 @@ import { AppModule } from './app.module';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
-  await app.listen(3000, '192.168.12.37');
+  await app.listen(3000);
 }
 bootstrap();
